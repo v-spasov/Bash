c010d8679e18435fe789064cfc96c67a2297b0a8
Vladimir Spasov <spasov.us@gmail.com>
2020-03-03 19:12:58 -0600

    Update AWSApache.sh

--- AWSApache.sh
+++ AWSApache.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Install Apache on Amazon Linux 2
+# Update Amazon Linux 2
 yum update -y
+
+# Install Apache, PHP and MariaDB on Amazon Linux 2
 amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum install -y httpd mariadb-server
 systemctl start httpd
+
+# Start Apache on system boot
 systemctl enable httpd
 
 # Create index.html
