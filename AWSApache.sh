bee74b4c5f4a11b97e9c8c9a64aa94800167d171
Vladimir Spasov <spasov.us@gmail.com>
2020-03-03 19:10:15 -0600

    Update AWSApache.sh

--- AWSApache.sh
+++ AWSApache.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Install Apache on Amazon Linux 2
 yum update -y
 amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum install -y httpd mariadb-server
 systemctl start httpd
 systemctl enable httpd
+
+# Create index.html
+cd /var/www/html
+echo "<html><h1>Welcome!</h1></html>"  >  index.html
